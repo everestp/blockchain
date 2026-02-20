c26c730609cd11f3a93358f4c109a70299b7aa8f
everestp <paudeleverest09@gmail.com>
2026-02-20 23:00:27 +0545

    Searching Block

--- src/blockchain/mod.rs
+++ src/blockchain/mod.rs
@@ -1,107 +1,169 @@
-
 use std::{time::SystemTime, vec};
 
 use sha2::{Digest, Sha256};
 
+pub enum BlockSearch {
+    SearchByIndex(usize),
+    SearchByPreviousHash(Vec<u8>),
+    SearchByBlockHash(Vec<u8>),
+    SearchByTimeStamp(u128),
+    SearchByTransaction(Vec<u8>),
+    SearchByNonce(i32),
+}
 
+pub enum BlockSearchResult<'a> {
+    Success(&'a Block),
+    FailOfEmptyBlocks,
+    FailOfIndex(usize),
+    FailOfPreviousHash(Vec<u8>),
+    FailOfBlockHash(Vec<u8>),
+    FailOfNonce(i32),
+    FailOfTimeStamp(u128),
+    FailOfTransaction(Vec<u8>),
+}
 
 #[derive(Debug)]
- pub struct  Block{
-    nonce:i32,
-    previous_hash:Vec<u8>,
-    time_stamps :u128,
-    transactions:Vec<Vec<u8>>
-
+pub struct Block {
+    nonce: i32,
+    previous_hash: Vec<u8>,
+    time_stamps: u128,
+    transactions: Vec<Vec<u8>>,
 }
+
 impl Block {
-    //method for the struct , class methods
-    //Two kind of method ,
-    //One kind ->Static method = which  not reading or writing  into field of the block
-
-    //Self is alias name for object , if we change the name of struct 
-    //then we do not  need to change the name inside here
-    pub fn new(nonce:i32 , previous_hash:Vec<u8>)-> Self{
-        //this method will take control; of the input of previous_hash
-         let time_now = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap();
-         Block { 
-            nonce:nonce,
-            previous_hash:previous_hash,
-            time_stamps:time_now.as_nanos(),
-            transactions : Vec::<Vec<u8>>::new()
-
-          }
-
+    pub fn new(nonce: i32, previous_hash: Vec<u8>) -> Self {
+        let time_now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap();
+        Block {
+            nonce,
+            previous_hash,
+            time_stamps: time_now.as_nanos(),
+            transactions: Vec::<Vec<u8>>::new(),
+        }
     }
 
-// struct method which is need to read or write to  field to stuct
-//self  which reference to the struct instance
- pub fn print(&self){
-    //format value of hex
-    println!("nonce : {}",self.nonce);
+    pub fn print(&self) {
+        println!("nonce : {}", self.nonce);
+        println!("Timestamp :{:x}", self.time_stamps);
+        println!("previous hash :{:?}", self.previous_hash);
+        println!("transaction:{:?}", self.transactions);
+    }
 
-    println!("Timestamp :{:x}",self.time_stamps);
-
-    //print vector , ask thje compiler  to do it
-    println!("previous haxsh :{:?}",self.previous_hash);
-    println!(" transaction:{:?}",self.transactions);
-
-}
-  pub fn hash(&self)-> Vec<u8>{
+    pub fn hash(&self) -> Vec<u8> {
         let mut bin = Vec::<u8>::new();
         bin.extend(self.nonce.to_be_bytes());
         bin.extend(self.previous_hash.clone());
         bin.extend(self.time_stamps.to_be_bytes());
-        for tx in self.transactions.iter(){
+        for tx in self.transactions.iter() {
             bin.extend(tx.clone());
         }
 
-     let mut hasher = Sha256::new();
-     hasher.update(bin);
-
-     return hasher.finalize().to_vec();
-     
+        let mut hasher = Sha256::new();
+        hasher.update(bin);
+        hasher.finalize().to_vec()
     }
-
 }
 
 #[derive(Debug)]
- pub struct  BlockChain{
-    transaction_pool : Vec<Vec<u8>>,
-    chain : Vec<Block>,
+pub struct BlockChain {
+    transaction_pool: Vec<Vec<u8>>,
+    chain: Vec<Block>,
 }
 
-impl BlockChain{
-   pub fn new() -> Self {
-        let mut bc = BlockChain { 
-            transaction_pool : Vec::<Vec<u8>>::new(),
-            chain:Vec::<Block>::new(),
+impl BlockChain {
+    pub fn new() -> Self {
+        let mut bc = BlockChain {
+            transaction_pool: Vec::<Vec<u8>>::new(),
+            chain: Vec::<Block>::new(),
         };
+        bc.create_block(0, vec![0 as u8, 32]);
+        bc
+    }
 
-        bc.create_block(0 , vec![0 as u8 ,32]);
-        return bc;
-
-    } 
-
-   pub  fn create_block(&mut self , nonce:i32 , previous_hash:Vec<u8>){
+    pub fn create_block(&mut self, nonce: i32, previous_hash: Vec<u8>) {
         let b = Block::new(nonce, previous_hash);
         self.chain.push(b);
     }
-    pub fn print(&self){
-        // using  iterator  to loop over the vector
-        for(i ,block) in self.chain.iter().enumerate(){
-           println!(" {} Chain {} {}","=".repeat(25),i,"=".repeat(25));
-           block.print();
+
+    pub fn print(&self) {
+        for (i, block) in self.chain.iter().enumerate() {
+            println!(" {} Chain {} {}", "=".repeat(25), i, "=".repeat(25));
+            block.print();
         }
-        println!("{}","*".repeat(25))
+        println!("{}", "*".repeat(25));
     }
 
-     pub fn  last_block(&self) -> &Block{
+    pub fn last_block(&self) -> &Block {
         if self.chain.len() > 1 {
-            return  &self.chain[self.chain.len()-1];
-        } else{
-            return  &self.chain[0];
+            &self.chain[self.chain.len() - 1]
+        } else {
+            &self.chain[0]
         }
     }
 
-  
+    pub fn search_block(&self, search: BlockSearch) -> BlockSearchResult {
+        for (idx, block) in self.chain.iter().enumerate() {
+            match search {
+                BlockSearch::SearchByIndex(index) => {
+                    if idx == index {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfIndex(index);
+                    }
+                }
+
+                BlockSearch::SearchByPreviousHash(ref hash) => {
+                    if block.previous_hash == *hash {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfPreviousHash(hash.to_vec());
+                    }
+                }
+
+                BlockSearch::SearchByBlockHash(ref hash) => {
+                    if block.hash() == *hash {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfBlockHash(hash.to_vec());
+                    }
+                }
+
+                BlockSearch::SearchByTimeStamp(ts) => {
+                    if block.time_stamps == ts {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfTimeStamp(ts);
+                    }
+                }
+
+                BlockSearch::SearchByTransaction(ref transaction) => {
+                    let mut found = false;
+                    for t in block.transactions.iter() {
+                        if t == transaction {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if found {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfTransaction(transaction.to_vec());
+                    }
+                }
+
+                BlockSearch::SearchByNonce(nonce) => {
+                    if block.nonce == nonce  {
+                        return BlockSearchResult::Success(block);
+                    }  if idx >= self.chain.len() {
+                        return BlockSearchResult::FailOfNonce(nonce);
+                    }
+                }
+            }
+        }
+
+    
+
+        BlockSearchResult::FailOfEmptyBlocks
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,61 @@
 pub mod blockchain;
 
-use blockchain::BlockChain;
+use blockchain::{Block , BlockChain , BlockSearch,BlockSearchResult};
 use sha2::{Digest, Sha256};
 
+fn get_block_search_result(result: BlockSearchResult) {
+    match result {
+        BlockSearchResult::Success(block) => {
+            println!(" Found the block:");
+            block.print();
+        }
+
+
+        BlockSearchResult::FailOfIndex(idx) => {
+            println!(" No block found at index: {}", idx);
+        }
+
+        BlockSearchResult::FailOfPreviousHash(hash) => {
+            println!(" No block found with previous hash: {:?}", hash);
+        }
+
+        BlockSearchResult::FailOfBlockHash(hash) => {
+            println!(" No block found with block hash: {:?}", hash);
+        }
+
+        BlockSearchResult::FailOfNonce(nonce) => {
+            println!(" No block found with nonce: {}", nonce);
+        }
+
+        BlockSearchResult::FailOfTimeStamp(ts) => {
+            println!(" No block found with timestamp: {}", ts);
+        }
+
+        BlockSearchResult::FailOfTransaction(tx) => {
+            println!(" No block found containing transaction: {:?}", tx);
+        }
+
+        BlockSearchResult::FailOfEmptyBlocks => {
+            println!("⚠️ The blockchain has no blocks.");
+        }
+    }
+}
 fn main() {
-    let mut hasher = Sha256::new();
-    hasher.update(b"hello world \n");
-    let result = hasher.finalize();
-    println!("Hash result :{:x}", result);
+  
 
     let mut block_chain = BlockChain::new();
     println!("Block chain :{:?}", block_chain);
     block_chain.print();
 
     let previous_hash = block_chain.last_block().hash();
-
+      let hash_to_find = previous_hash.clone();
+    
     block_chain.create_block(1, previous_hash);
     let previous_hash = block_chain.last_block().hash();
     block_chain.create_block(2, previous_hash);
     block_chain.print();
+    let  result = block_chain.search_block(BlockSearch::SearchByIndex(1));
+    get_block_search_result(result);
+     let result1 = block_chain.search_block(BlockSearch::SearchByIndex(10));
+    get_block_search_result(result1);
 }
