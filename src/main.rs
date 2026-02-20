6e150e26eb64ad2caee37e7bcdf9b1952e392355
everestp <paudeleverest09@gmail.com>
2026-02-20 20:32:55 +0545

    chain to link all blocks

--- src/main.rs
+++ src/main.rs
@@ -46,12 +46,50 @@ fn print(&self){
 
 }
 
+#[derive(Debug)]
+struct  BlockChain{
+    transaction_pool : Vec<Vec<u8>>,
+    chain : Vec<Block>,
+}
+
+impl BlockChain{
+    fn new() -> Self {
+        let mut bc = BlockChain { 
+            transaction_pool : Vec::<Vec<u8>>::new(),
+            chain:Vec::<Block>::new(),
+        };
+
+        bc.create_block(0 , "Hash for very first block ".to_string().into_bytes());
+        return bc;
+
+    } 
+
+    fn create_block(&mut self , nonce:i32 , previous_hash:Vec<u8>){
+        let b = Block::new(nonce, previous_hash);
+        self.chain.push(b);
+    }
+    fn print(&self){
+        // using  iterator  to loop over the vector
+        for(i ,block) in self.chain.iter().enumerate(){
+           println!(" {} Chain {} {}","=".repeat(25),i,"=".repeat(25));
+           block.print();
+        }
+        println!("{}","*".repeat(25))
+    }
+}
+
 fn main() {
 
-    let b = Block::new(0, "this is first block".to_string().into_bytes());
-    println!("Hello, world!");
-    b.print();
-    println!("the first block is :{:?}",b);
+    // let b = Block::new(0, "this is first block".to_string().into_bytes());
+    // println!("Hello, world!");
+    // b.print();
+    // println!("the first block is :{:?}",b);
+    let mut block_chain = BlockChain::new();
+    println!("Block chain :{:?}",block_chain);
+    block_chain.print();
+    block_chain.create_block(1, "hash-1".to_string().into_bytes());
+     block_chain.create_block(2, "hash-2".to_string().into_bytes());
+      block_chain.print();
 }
 
 
